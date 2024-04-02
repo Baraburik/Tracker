a369e30e87562a873040fd2d27694f0b7ad3f808
Baraburik <pavelkuts2006@mail.ru>
2024-04-02 13:36:11 +0300

    init

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
+import cv2
 from ultralytics import YOLO
 from deep_sort_realtime.deepsort_tracker import DeepSort
 
 tracker = DeepSort(max_age=5)
 
-video_path = 'rtsp://admin:QWEqwe123@192.168.1.64:554/ISAPI/Streaming/Channels/101' #
+video_path = 'rtsp://admin:QWEqwe123@192.168.1.64:554/ISAPI/Streaming/Channels/101'
 model = YOLO('yolov8n.pt')  # put your weights
-results = model(video_path, show=True)
 
-for result in results:
-    boxes = result.boxes  # Boxes object for bounding box outputs
-    masks = result.masks  # Masks object for segmentation masks outputs
-    keypoints = result.keypoints  # Keypoints object for pose outputs
-    probs = result.probs  # Probs object for classification outputs
+cap = cv2.VideoCapture(video_path)
 
-    bbs = result.detect(video_path)
-    tracks = tracker.update_tracks(bbs, frame=video_path)
-    for track in tracks:
-        if not track.is_confirmed():
-            continue
-        track_id = track.track_id
-        ltrb = track.to_ltrb()
+while cap.isOpened():
+    success, frame = cap.read()
+
+    if success:
+        results = model.track(frame, persist=True)
+
+        annotated_frame = results[0].plot()
+
+        cv2.imshow("Frame", annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
